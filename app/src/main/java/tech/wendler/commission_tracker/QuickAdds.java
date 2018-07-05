35583ca735ec1e34269f44538e8a425b0a589e7d
Nick Wendler <nick@wendler.tech>
2018-07-05 14:37:23 -0600

    Added function of saving new transaction to a sqlite DB. Only save is implemented.

--- app/src/main/java/tech/wendler/commission_tracker/QuickAdds.java
+++ app/src/main/java/tech/wendler/commission_tracker/QuickAdds.java
@@ -5,9 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class QuickAdds extends Fragment {
+
+    private Button btnPlusNewPhone, btnMinusNewPhone, btnMinusUpg, btnPlusUpg, btnMinusBundle,
+            btnPlusBundle, btnMinusCD, btnPlusCD, btnMinusTablet, btnPlusTablet, btnSubmit;
+
     public QuickAdds() {
         // Required empty public constructor
     }
@@ -27,5 +32,10 @@ public class QuickAdds extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_quick_adds, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 }
